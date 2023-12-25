0bd40cf96bdc1fdeb9c1981dd2c88c170b0efb93
LarisaYakovenko <133209659+LarisaYakovenko@users.noreply.github.com>
2023-12-25 19:59:57 +0200

    fixed file

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import css from './Home.module.css';
 export default function Home() {
   return (
     <div className={css.container}>
-      <h1 className={css.title}>Phonebook</h1>
+      <h1 className={css.title}>Welcome to Phonebook</h1>
       <div></div>
     </div>
   );
